430fea6218c00759e745793978a4559860af7bbc
Geda-Seifu <gadaa.official00@gmail.com>
2025-04-24 00:28:41 +0300

    count odd in a range

---
+++ countOdd.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        lenght = high - low + 1
+        count = lenght // 2
+        if lenght % 2  and low % 2:
+            count += 1
+        return count
+    
+    print
